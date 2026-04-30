915164dbb592163e197aa0e3b1241cae4691f87b
Jeno Pekarjuk <epekaryuk@outlook.com>
2026-04-30 22:39:49 +0200

    setting time instead using read-only properties

--- TimeCalculator/Core/Types/TimeEntry.cs
+++ TimeCalculator/Core/Types/TimeEntry.cs
@@ -10,4 +10,22 @@ public class TimeEntry
 
     [JsonPropertyName("description")]
     public string Description { get; set; } = string.Empty;
+
+    public int Hours
+    {
+        get => Time.Hours;
+        set => Time = new TimeSpan(value, Time.Minutes, Time.Seconds);
+    }
+
+    public int Minutes
+    {
+        get => Time.Minutes;
+        set => Time = new TimeSpan(Time.Hours, value, Time.Seconds);
+    }
+
+    public int Seconds
+    {
+        get => Time.Seconds;
+        set => Time = new TimeSpan(Time.Hours, Time.Minutes, value);
+    }
 }
